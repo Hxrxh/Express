d6dd03529736de2ab114339d868c4366c149c23a
Hxrxh <harshbhardwaj4556>
2025-09-14 02:09:28 +0530

    query and params

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require("express");
 
 const app = express();
-
-app.get("/products", (req, res, next) => {
-  res.send("Here is the list of all products.");
-});
-app.post("/products", (req, res, next) => {
-  res.send("A new product has been added.");
+app.get("/welcome/:username", (req, res, next) => {
+  const username = req.params.username;
+  const role = req.query.role;
+  res.send(`Welcome ${username},your role is ${role}`);
 });
 
-app.get("/categories", (req, res, next) => {
-  res.send("Here is the list of all categories.");
-});
-app.post("/categories", (req, res, next) => {
-  res.send("A new category has been created.");
-});
-app.use((req, res, next) => {
-  res.status(404).send("<h1>404-Page Not Found</h1>");
-});
 app.listen(4000, () => {
   console.log(`server is running on local host 4000`);
 });

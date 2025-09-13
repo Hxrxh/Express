fa58ffede3e5fc3f91d9eef75c2456ec0314edd2
Hxrxh <harshbhardwaj4556>
2025-09-13 22:52:18 +0530

    new property to request object

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log(
-    "Server is up and running on port 3000! Ready to handle requests."
-  );
+  req.userType = "Admin";
   next();
 });
-app.use((req, res, next) => {
-  res.send("<h1>My name is Harsh</h1>");
+
+app.get("/welcome", (req, res, next) => {
+  res.send(`<h1>Welcome , ${req.userType} </h1>`);
 });
 app.listen(3000);
